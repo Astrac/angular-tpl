6c8927d6956316b1056ad636c255027e20a48e7e
Aldo Stracquadanio <aldo.strac@gmail.com>
2013-07-08 17:21:42 +0100

    Basic template for an angular-js app
    Including:
    
    - Yeoman
    - Grunt
    - Karma
    - Bower
    - LESS
    - Twitter Bootstrap LESS
    - AngularJS
    - AngularJS LocalStorageService plugin
    - AngularJS HTTP-Auth (my fork)
    - JS Hashes
    - Underscore JS

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularTplApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTplApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
